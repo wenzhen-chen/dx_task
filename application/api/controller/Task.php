f29023154733f1e0e052d5278bb44e491e2505bb
wenzhen-chen <18259656089@163.com>
2019-08-30 18:26:03 +0800

    task控制器

---
+++ application/api/controller/Task.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/8/22
+ * Time: 21:01
+ */
+
+namespace app\api\controller;
+
+use app\common\controller\BaseController;
+use app\common\business\Task as taskBn;
+
+class Task extends BaseController
+{
+    /**
+     * 社群列表
+     */
+    public function groupTaskModule()
+    {
+        try {
+            $page = input('page', 1);
+            $pageSize = input('pageSize', 10);
+            $list = taskBn::getFontModuleList('type=1', $page, $pageSize);
+            $this->_echoSuccessMessage(
+                0, '获取成功', $list
+            );
+        } catch (\Exception $e) {
+            $this->_echoErrorMessage($e->getMessage(), $e->getCode());
+        }
+    }
+
+    /**
+     * 发布社群任务
+     */
+    public function groupPublish()
+    {
+        try {
+            $param = input();
+            //验证数据
+            $validate = Validate('Task');
+            $post = $validate->scene('groupTask')->check($param);
+            if (!$post) {
+                $this->_echoSuccessMessage('success', ['code' => -10001, 'msg' => $validate->getError()]);
+            }
+            unset($param['token']);
+            $param['module_id'] = $param['groupId'];//前端传groupId，后端存module_id
+            unset($param['groupId']);
+            $param['user_id'] = $this->_isLogin();
+            $result = taskBn::addGroupTask($param);
+            $this->_echoSuccessMessage($result['code'],$result['msg']);
+        } catch (\Exception $e) {
+            $this->_echoErrorMessage($e->getMessage(), $e->getCode());
+        }
+    }
+
+    /**
+     * 派发任务
+     */
+    public function getGroupTask(){
+        try {
+            $userId = $this->_isLogin();
+            $moduleId = input('groupId',0);
+            $taskInfo = taskBn::getMaxGroupTask($userId,$moduleId);
+            $this->_echoSuccessMessage($taskInfo['code'],$taskInfo['msg'],$taskInfo['data']);
+        } catch (\Exception $e) {
+            $this->_echoErrorMessage($e->getMessage(), $e->getCode());
+        }
+    }
+}
